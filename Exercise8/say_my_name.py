ea8de53b870b9075f1811c4104e28461ade1e389
Monica Amanfo <monica.amanfo1@gmail.com>
2025-01-26 17:29:30 +0000

    CREATE: completed exercise 8 part 1

---
+++ Exercise8/say_my_name.py
@@ -0,0 +1,9 @@
+first_name = "Sheldon"
+# "first_name" in this example is a variable. A variable stores data or information of various data types.
+# In this case the data stored is a string called "Sheldon".
+last_name = "Cooper"
+# "last_name" is another variable. In this case the variable is storing the string "Cooper".
+print(first_name + " " + last_name)
+# print is an in-built python function which outputs the arguments within the function and logs it to the run window/console.
+# In this particular print function is the first variable which is then concatenated with a string with a space,
+# and finally the second variable "last_name" which outputs "Sheldon Cooper" to the console.
