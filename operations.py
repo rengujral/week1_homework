e298d5743600eea0f0181aaf6339cec1b1f19eb9
Renee Gujral <reneegujral1@gmail.com>
2025-01-27 15:10:51 +0000

    EDIT: added code from exercise instruction

---
+++ operations.py
@@ -0,0 +1,11 @@
+# Get user input
+var = input("Please enter a value: ")
+
+# Print the value in uppercase
+print("Upper case:", var.upper())
+
+# Print the number of characters
+print("Number of characters:", len(var))
+
+# Check if the input contains numeric characters
+print("Contains numeric characters:", var.isdecimal())
